2cfa3984c91dd090932eba2887e250f2630e069a
Cameron Bytheway <cameron@theflokk.com>
2013-10-15 23:27:12 -0600

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ hyper-emitter
 =============
 
 Easily subscribe to hyper resources
+
+Usage
+-----
+
+```js
+var client = require('hyper-emitter');
+
+// Make a request to a resource
+client.get('/component.json', function(err, body) {
+  console.log('First subscriber', err, body);
+});
+
+// Another request to the same resource will only make 1 request
+var unsub = client.get('/component.json', function(err, body) {
+  console.log('Second subscriber', err, body);
+
+  // call `unsub` to unsubscribe from any changes
+  unsub();
+});
+
+setTimeout(function() {
+  // refresh the resource for all of the listeners
+  client.refresh('/component.json');
+}, 2000);
+```
---
+++ index.js
@@ -0,0 +1,115 @@
+/**
+ * Module dependencies
+ */
+
+var client = require('hyperagent');
+var Emitter = require('emitter');
+var each = require('each');
+
+/**
+ * Setup a singleton emitter
+ */
+
+var emitter = new Emitter();
+
+/**
+ * Get a resource and get called any time it changes
+ *
+ * @param {String} url
+ * @param {Function} fn
+ * @return {Function}
+ */
+
+exports.get = function(url, fn) {
+  // Proxy the fn so it can be used more than once
+  function handle(err, body) { fn(err, body); }
+
+  emitter.on(url, handle);
+  exports.refresh(url, true);
+
+  // Return a function to unsubscribe
+  return function unsubscribe() {
+    emitter.off(url, handle);
+  };
+};
+
+/**
+ * Submit a form and update any subscribers
+ *
+ * @param {String} method
+ * @param {String} action
+ * @param {Object} data
+ * @param {Function} fn
+ */
+
+exports.submit = function(method, action, data, fn) {
+  var lowerMethod = (method || 'GET').toLowerCase();
+
+  var req = client[lowerMethod](action);
+
+  if (lowerMethod === 'get') req.query(data);
+  else if (data) req.send(data);
+
+  req
+    .on('error', fn)
+    .end(function(res) {
+      fn(null, res.body);
+
+      if (lowerMethod === 'get') return;
+
+      // Hard refresh any resources that may have been affected
+      var urls = [action];
+      each(exports.refreshHeaders, function(header) {
+        var value = res.headers[header];
+        if (value && value !== action) urls.push(location);
+      });
+
+      // TODO look for http://tools.ietf.org/html/draft-nottingham-linked-cache-inv-04
+
+      each(urls, exports.refresh);
+    });
+};
+
+/**
+ * Save any urls that are in flight
+ */
+
+var inflights = {};
+
+/**
+ * Refresh a url and notify the subscribers
+ *
+ * @param {String} url
+ * @param {Boolean} useCache
+ */
+
+exports.refresh = function(url, useCache) {
+  // If someone is already making a request wait for theirs
+  if (inflights[url]) return;
+
+  inflights[url] = true;
+
+  var req = client.get(url);
+
+  if (!useCache) req.ignoreCache();
+
+  function handle(err, body, res) {
+    delete inflights[url];
+    emitter.emit(url, err, body);
+  }
+
+  req
+    .on('error', handle)
+    .end(function(res) {
+      handle(null, res.body, res);
+    });
+};
+
+/**
+ * Expose the list of headers to refresh
+ */
+
+exports.refreshHeaders = [
+  'location',
+  'content-location'
+];
